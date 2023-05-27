803481ea8a0b1c31f87bb8e26791186c5555d4a2
Md Najmul Haque <najmulhaque008@gmail.com>
2023-05-27 13:25:08 +0600

    create user services completed

--- services/userService.js
+++ services/userService.js
@@ -4,5 +4,17 @@ export const createUserServices = async (data) => {
     const { name, email, password, confirmPassword } = data
 
     const salt = await bcrypt.genSalt(12)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const doc = new User({
+        name: name,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword
+    })
+
+    const user = await doc.save()
+
+    return user;
 
 }
